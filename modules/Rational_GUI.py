a8a6f8e229df1a0fcb335a2d964f1282daf428df
Brynik1 <144445472+Brynik1@users.noreply.github.com>
2024-11-05 21:21:43 +0300

    Добавлена светлая тема

--- modules/Rational_GUI.py
+++ modules/Rational_GUI.py
@@ -4,32 +4,41 @@ from Natural import *
 from Integer import *
 from Rational import *
 from additionally import *
+import argparse
 
 
 class RationalApp:
-    def __init__(self, root):
-        # Определение цветов
-        self.bg_color = "#1e1f22"  # Цвет фона
-        self.window_color = "#2b2d30"  # Цвет окон
-        self.text_color = "white"  # Цвет текста
-        self.hover_color = "#3c3f42"  # Цвет при наведении
+    def __init__(self, root, theme='dark'):
+        # Определение цветов в зависимости от темы
+        if theme == 'light':
+            self.bg_color = "#FFFFFF"  # Цвет фона
+            self.window_color = "#EAEAEA"  # Цвет окон
+            self.text_color = "#333333"  # Цвет текста
+            self.hover_color = "#C0C0C0"  # Цвет при наведении
+            self.button_color = "#5ebf62"  # Цвет для кнопки
+        else:  # Темная тема по умолчанию
+            self.bg_color = "#232323"  # Цвет фона
+            self.window_color = "#3A3A3A"  # Цвет окон
+            self.text_color = "#F5F5F5"  # Цвет текста
+            self.hover_color = "#4B4B4B"  # Цвет при наведении
+            self.button_color = "#4CAF50"  # Цвет для кнопки
 
         self.root = root
-        self.root.title("Rational Number Operations")
-        self.root.geometry("374x350")
+        self.root.title("Rational Operations")
+        self.root.geometry("360x420")
         self.root.configure(bg=self.bg_color)
-        self.root.attributes('-alpha', 0.98)
+        self.root.attributes('-alpha', 1)
 
         self.method_var = tk.StringVar(value="Сложение дробей")
 
         # Заголовок
-        title_label = tk.Label(root, text="Операции с рациональными числами", bg=self.bg_color, fg=self.text_color, font=("Arial", 16))
+        title_label = tk.Label(root, text="Операции с рациональными", bg=self.bg_color, fg=self.text_color, font=("Arial", 16))
         title_label.pack(pady=10)
 
         # Выбор метода
         methods = [
             "Сокращение дроби",
-            "Проверка сокращенного на целое",
+            "Проверка на целое",
             "Целое -> дробное",
             "Дробное -> целое",
             "Сложение дробей",
@@ -44,32 +53,37 @@ class RationalApp:
         tk.Label(method_frame, text="Выберите метод:", bg=self.bg_color, fg=self.text_color, font=("Arial", 10)).pack(side=tk.LEFT)
 
         self.method_menu = tk.OptionMenu(method_frame, self.method_var, *methods)
-        self.method_menu.config(bg=self.window_color, fg=self.text_color, highlightbackground=self.window_color)
+        self.method_menu.config(bg=self.bg_color, fg=self.text_color, highlightbackground=self.window_color, relief=tk.FLAT)
 
         # Настройка событий для изменения цвета фона
         self.method_menu.bind("<Enter>", lambda e: self.method_menu.config(bg=self.hover_color))
-        self.method_menu.bind("<Leave>", lambda e: self.method_menu.config(bg=self.window_color))
+        self.method_menu.bind("<Leave>", lambda e: self.method_menu.config(bg=self.bg_color))
         self.method_menu.pack(side=tk.LEFT)
 
         # Ввод первого числа
         tk.Label(root, text="Введите первую дробь:", bg=self.bg_color, fg=self.text_color, font=("Arial", 10)).pack(pady=5)
-        self.first_number_entry = tk.Entry(root, bg=self.window_color, fg=self.text_color, insertbackground='white', font=("Arial", 14))
+        self.first_number_entry = tk.Entry(root, bg=self.window_color, fg=self.text_color, width=26, insertbackground='black' if theme == 'light' else 'white', font=("Arial", 14))
         self.first_number_entry.pack(pady=5)
 
         # Ввод второго числа
         tk.Label(root, text="Введите вторую дробь (если необходимо):", bg=self.bg_color, fg=self.text_color, font=("Arial", 10)).pack(pady=5)
-        self.second_number_entry = tk.Entry(root, bg=self.window_color, fg=self.text_color, insertbackground='white', font=("Arial", 14))
+        self.second_number_entry = tk.Entry(root, bg=self.window_color, fg=self.text_color, width=26, insertbackground='black' if theme == 'light' else 'white', font=("Arial", 14))
         self.second_number_entry.pack(pady=5)
+
+        # Ввод цифры
+        tk.Label(root, text="Введите цифру (для методов с цифрой):", bg=self.bg_color, fg=self.text_color, font=("Arial", 10)).pack(pady=5)
+        self.digit_entry = tk.Entry(root, bg=self.window_color, fg=self.text_color, insertbackground='black' if theme == 'light' else 'white', font=("Arial", 14))
+        self.digit_entry.pack(pady=5)
 
         # Метка для результата
         self.result_label = tk.Label(root, text="", bg=self.bg_color, fg=self.text_color, font=("Arial", 14))
         self.result_label.pack(pady=10)
 
         # Кнопка для выполнения операции
-        self.calculate_button = tk.Button(root, text="Выполнить", command=self.calculate, bg="#4CAF50", fg=self.text_color, font=("Arial", 14), height=1, width=15)
+        self.calculate_button = tk.Button(root, text="Выполнить", command=self.calculate, bg=self.button_color, fg="white", font=("Arial", 14), height=1, width=15, relief=tk.FLAT)
         self.calculate_button.pack(pady=10)
         self.calculate_button.bind("<Enter>", lambda e: self.calculate_button.config(bg="#61e867"))
-        self.calculate_button.bind("<Leave>", lambda e: self.calculate_button.config(bg="#4CAF50"))
+        self.calculate_button.bind("<Leave>", lambda e: self.calculate_button.config(bg=self.button_color))
 
     def calculate(self):
         method_name = self.method_var.get()
@@ -116,7 +130,7 @@ class RationalApp:
                 result = first_number.RED_Q_Q()
                 self.result_label.config(text=f"{first_number} = {result}")
 
-            elif method_name == "Проверка сокращенного на целое":
+            elif method_name == "Проверка на целое":
                 result = first_number.INT_Q_B()
                 if result: self.result_label.config(text=f"Является целым")
                 else: self.result_label.config(text=f"Не является целым")
@@ -139,6 +153,13 @@ class RationalApp:
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Polynomial Operations App')
+    parser.add_argument('--theme', type=str, choices=['light', 'dark'], default='dark',
+                        help='Choose the theme of the application')
+
+    args = parser.parse_args()
+
     root = tk.Tk()
-    app = RationalApp(root)
+    app = RationalApp(root, theme=args.theme)  # Передача темы из аргументов командной строки
     root.mainloop()
+
